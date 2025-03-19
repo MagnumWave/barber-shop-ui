41a2faffb9257ee4eccbf3e4c98362b895ce5918
MagnumWave <aureliomagno@terra.com.br>
2025-03-19 17:48:29 -0300

    edit-client feito e lógica criada nos componentes

--- src/app/commons/components/card-header/card-header.component.ts
+++ src/app/commons/components/card-header/card-header.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+//import { MatCard, MatCardTitle, MatCardHeader } from '@angular/material/card';
+//meu auto-complete não pegou o modulo então tive que importar um por um antes.
+//entendo que importar o módulo todo tenha suas desvantagens em tamanho de aplicação.
 
 @Component({
   selector: 'app-card-header',
-  imports: [],
+  imports: [MatCardModule],
   templateUrl: './card-header.component.html',
   styleUrl: './card-header.component.scss'
 })
--- src/app/services/snackbar-manager.service.ts
+++ src/app/services/snackbar-manager.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { ISnackbarManagerService } from './isnackbar-manager.service';
+import { MatSnackBar } from '@angular/material/snack-bar'
 
 @Injectable({
   providedIn: 'root'
 })
-export class SnackbarManagerService {
+export class SnackbarManagerService implements ISnackbarManagerService {
 
-  constructor() { }
+  constructor(
+    private readonly snackbar: MatSnackBar
+  ) { }
+
+  show(message: string, action: string = 'fechar', duration: number = 3000): void {
+    this.snackbar.open(message, action, { duration, verticalPosition: 'top', horizontalPosition: 'right'});
+  }
 }
