ff7c7e3e6afe4e04cb0a0432214ad496baf3c301
MagnumWave <aureliomagno@terra.com.br>
2025-03-20 18:32:14 -0300

    schedule-service criada

---
+++ src/app/clients/components/client-table/custom-paginator.ts
@@ -0,0 +1,15 @@
+import { Injectable } from "@angular/core";
+import { MatPaginatorIntl } from "@angular/material/paginator";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CustomPaginator extends MatPaginatorIntl {
+
+  override itemsPerPageLabel = 'Itens por página';
+  override nextPageLabel = 'Próxima página';
+  override previousPageLabel = 'Página Anterior';
+  override firstPageLabel = 'Primeira página';
+  override lastPageLabel = 'Última página';
+
+}
---
+++ src/app/services/idialog-manager.service.ts
@@ -0,0 +1,9 @@
+import { ComponentType } from "@angular/cdk/portal";
+import { YesNoDialogComponent } from "../commons/components/yes-no-dialog/yes-no-dialog.component";
+import { Observable } from "rxjs";
+
+export interface IDialogmanagerService {
+
+  showYesNoDialog(component: ComponentType<YesNoDialogComponent>, data: { title: string, content: string}): Observable<any>
+
+}
