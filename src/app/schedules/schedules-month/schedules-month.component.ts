750bc219dff30c176120dc56e5f215f3f4957aa1
MagnumWave <aureliomagno@terra.com.br>
2025-03-21 15:41:08 -0300

    tela de agendamento quase pronta

--- src/app/schedules/schedules-month/schedules-month.component.ts
+++ src/app/schedules/schedules-month/schedules-month.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { ScheduleCalendarComponent } from "../components/schedule-calendar/schedule-calendar.component";
 
 @Component({
   selector: 'app-schedules-month',
-  imports: [],
+  imports: [ScheduleCalendarComponent],
   templateUrl: './schedules-month.component.html',
   styleUrl: './schedules-month.component.scss'
 })
